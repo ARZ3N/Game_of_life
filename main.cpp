b15a64416521d1ddeac8e2ef27d1dd6720635316
ARZ3N <aniZ3n99@outlook.com>
2021-11-09 12:54:53 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+//#include<conio>
+#include<string>
+#include<vector>
+
+
+
+
+#define BRD_DIM 20
+
+void init_brd(char arr[][BRD_DIM]){
+    for(int i=0; i<BRD_DIM; i++){
+        for(int j=0; j<BRD_DIM; j++){
+            arr[i][j] = '.';
+        }
+    }
+}
+void print_brd(char arr[][BRD_DIM]){
+    for(int i=0;i<BRD_DIM; i++){
+        for(int j=0; j<BRD_DIM; j++){
+            std::cout<<arr[i][j]<<" ";
+        }
+        std::cout<<"\n";
+    }
+}
+void parser(std::string& usrStr, std::vector<std::string> &vec){
+    int position=0;
+    std::string temp;
+    //48 -> '0'
+    //57 -> '9'
+    //[arg1] [x,y r,b ] 
+/**
+ * proposed usr input-
+ * [no of arguments] -n[no. of cell inputs] -sym[symbol to represent live cell] [[input coordinates of live cells]]
+ */
+    while(position<usrStr.length()){
+        if(usrStr[position]!=' '){
+            temp += usrStr[position];
+            if(position == usrStr.length()-1){
+                //std::cout<<temp<<"position= "<<position<<"\n";
+                vec.push_back(temp);
+                temp.clear();    
+            }
+        }
+        else{
+            //std::cout<<temp<<"position= "<<position<<"\n";
+            vec.push_back(temp);
+            temp.clear();
+        }
+        position += 1;
+    }
+    
+
+}
+ int main(){
+    char board[BRD_DIM][BRD_DIM];
+    char ch;
+    std::string usrCmd="";
+    std::vector<std::string> args;
+    std::cout<<"/> -Board Init-"<<"\n";
+    init_brd(board);
+    print_brd(board);
+    std::cout<<"/> Input Command--"<<">> ";
+    std::getline(std::cin, usrCmd);
+    parser(usrCmd, args);
+    
+
+    usrCmd.clear();
+    args.clear();
+    return 0;
+ }
